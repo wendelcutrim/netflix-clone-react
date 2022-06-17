de41cbae827a0d4c36a8ff77b34ecfc3378d2572
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-06-17 15:44:57 -0300

    Exemplos sobre como utilizar o React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
 
 function App() {
+  const nome = "Wendel";
+  const title = "Teste";
+  const idade = 15;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>{title}{idade >= 18 ? "Maior" : "Menor"}</header>
+      <main>
+        <h1 className={"vermelho"}>Olá {nome}</h1>
+      </main>
     </div>
   );
 }
+
+
 
 export default App;
